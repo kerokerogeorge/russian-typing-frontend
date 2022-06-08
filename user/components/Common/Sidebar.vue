64dccd1886f6d05c07589f560118c67a00ad9c68
kerokerogeorge <kerokerogeorge@yahoo.co.jp>
2022-06-08 22:55:53 +0900

    ゲームの種類を切り替えられるように変更

--- user/components/Common/Sidebar.vue
+++ user/components/Common/Sidebar.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="sidebar">
     <div class="sidebar__container">
-      <div class="sidebar__container__content">
-        1
+      <div class="sidebar__container__content" :class="{'current': $route.query.type === 'letter'}">
+        <NuxtLink :to="{'path':'/game', 'query':{type: 'letter'}}">
+          一文字練習
+        </NuxtLink>
       </div>
-      <div class="sidebar__container__content">
-        2
+      <div class="sidebar__container__content" :class="{'current': $route.query.type === 'level-one'}">
+        <NuxtLink :to="{'path':'/game', 'query':{type: 'level-one'}}">
+          レベル1（~4文字）
+        </NuxtLink>
       </div>
-      <div class="sidebar__container__content">
-        3
+      <div class="sidebar__container__content" :class="{'current': $route.query.type === 'level-two'}">
+        <NuxtLink :to="{'path':'/game', 'query':{type: 'level-two'}}">
+          レベル2（5~7文字）
+        </NuxtLink>
       </div>
+      <div class="sidebar__container__content" :class="{'current': $route.query.type === 'level-three'}">
+        <NuxtLink :to="{'path':'/game', 'query':{type: 'level-three'}}">
+          レベル3（8~10文字）
+        </NuxtLink>
+      </div>
+      <div class="sidebar__container__content" :class="{'current': $route.query.type === 'level-four'}">
+        <NuxtLink :to="{'path':'/game', 'query':{type: 'level-four'}}">
+          レベル4（11文字以上）
+        </NuxtLink>
+      </div>
+      <!--  TODO: 将来的に追加 -->
+      <!-- <div class="sidebar__container__content" :class="{'current': $route.query.type === 'sentence'}" @click="selectType('sentence')">
+        <NuxtLink :to="{'path':'/game', 'query':{type: 'sentence'}}">
+          文章で練習
+        </NuxtLink>
+      </div> -->
     </div>
   </div>
 </template>
-
-<script>
-</script>
 
 <style lang="scss" scoped>
 @mixin pcL {
@@ -52,10 +71,23 @@
       width: 100%;
       cursor: pointer;
       box-sizing: border-box;
-      padding: 20px 25px;
       &:hover {
         background-color: rgb(243, 255, 249);
       }
+
+      a {
+        padding: 5px 25px;
+        display: block;
+        width: 100%;
+        height: 100%;
+        color: #323232;
+        // background-color: #b77b7b;
+        text-decoration: none;
+        line-height: 300%;
+      }
+    }
+    .current {
+      background-color: rgb(229, 252, 241);
     }
   }
 }
